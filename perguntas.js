1e15a5ac3556f3cb68e9103b850e55d0a7fc9930
arthurpp04 <00001118744421sp@al.educacao.sp.gov.br>
2024-11-28 23:42:04 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Matemática',
+    'O que são números perfeitos?',
+    'Números perfeitos são aqueles que são iguais à soma de seus divisores próprios. Um exemplo é 6, pois 1 + 2 + 3 = 6.'
+)
+
+criaCartao(
+    'Astronomia',
+    'O que é uma supernova?',
+    'Uma supernova é a explosão de uma estrela que libera uma quantidade enorme de energia, muitas vezes brilhando mais do que uma galáxia inteira por um curto período de tempo.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o rio mais longo do mundo?',
+    'O rio mais longo do mundo é o rio Nilo.'
+)
+
+criaCartao(
+    'Cinema',
+    'Qual filme ganhou o Oscar de Melhor Filme em 2020?',
+    'O filme "Parasita" ganhou o Oscar de Melhor Filme em 2020.'
+)
